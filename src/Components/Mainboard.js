d02be29f3ac1fa3c596177ede25b73f5499fd6af
Abhishek Singh <writetoabhishek07@gmail.com>
2021-02-20 05:35:49 +0530

    complete Navbar

---
+++ src/Components/Mainboard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import Pin from './Pin'
+
+function Mainboard() {
+    return (
+        <Wrapper>
+            <Container>
+                <Pin/>
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default Mainboard
+
+const Wrapper = styled.div`
+    background-color:white;
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    margin-top: 15px;
+`
+const Container = styled.div`
+    display: flex;
+    width: 80%;
+    background-color:white;
+`
---
+++ src/Components/Pin.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Pin() {
+    return (
+        <Wrapper>
+            <Container>
+
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default Pin
+
+const Wrapper = styled.div`
+
+`
+
+const Container = styled.div`
+
+`
